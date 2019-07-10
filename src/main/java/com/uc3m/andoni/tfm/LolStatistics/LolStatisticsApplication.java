3aaa1f8444154df1f00f41eb34c9932c87ed106b
Andoni Alcelay Izarzugaza <aalcelay@gmv.com>
2019-07-10 11:58:40 +0200

    Add project

---
+++ src/main/java/com/uc3m/andoni/tfm/LolStatistics/LolStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.uc3m.andoni.tfm.LolStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolStatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uc3m/andoni/tfm/LolStatistics/LolStatisticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uc3m.andoni.tfm.LolStatistics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LolStatisticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
